41be9faae61fbc2aeb2898a61256a227997e0bcc
Likitha96 <109337892+Likitha96@users.noreply.github.com>
2023-02-02 11:36:48 +0530

    initial commit for SpringBoot

--- src/main/java/com/example/demo/EmployeeController.java
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -1,12 +1,10 @@
 package com.example.demo;
 import com.example.demo.Exception.ResourceNotFoundException;
 import com.example.demo.Repository.EmployeeRepository;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 @CrossOrigin("*")
